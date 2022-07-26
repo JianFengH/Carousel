2f0ac7306946a9dc141dfef77574f79d53b44c14
JianFengH <707156268@qq.com>
2022-07-25 20:38:54 -0700

    Add Component CarouselCollection

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Autocomplete from './components/Autocomplete/Autocomplete';
 import Counter2 from './components/Counter/Counter2';
 import TodoList2 from './components/TodoList/TodoList2';
 import Autocomplete2 from './components/Autocomplete/Autocomplete2';
+import CarouselColletion, { CarouselColletionItem } from './components/Carousel/CarouselColletion';
 
 function App() {
   return (
@@ -28,9 +29,18 @@ function App() {
       <hr />
       <Counter2 />
       <TodoList2 />
-      
+
       <h2>Autocomplete function component</h2>
       <Autocomplete2 focus onSelectItem={(v) => alert(v)} />
+
+      <h2>CarouselColletion</h2>
+      <CarouselColletion>
+        <CarouselColletionItem style={{background: 'red'}}>1</CarouselColletionItem>
+        <CarouselColletionItem style={{background: 'blue'}}>2</CarouselColletionItem>
+        <CarouselColletionItem style={{background: 'green'}}>3</CarouselColletionItem>
+        <CarouselColletionItem style={{background: 'purple'}}>4</CarouselColletionItem>
+        <CarouselColletionItem style={{background: 'gray'}}>5</CarouselColletionItem>
+      </CarouselColletion>
     </div>
   );
 }
---
+++ src/components/Carousel/CarouselColletion.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import './CarouselCollection.css';
+
+CarouselColletionItem.propTypes = {
+  newClass: PropTypes.string,
+  children: PropTypes.any.isRequired,
+  style: PropTypes.object,
+};
+export function CarouselColletionItem({ children, newClass, style }) {
+  return <div className={cx({
+    'cc-item': true,
+    [newClass]: true,
+  })} style={style}>
+    {children}
+  </div>
+}
+
+CarouselColletion.propTypes = {
+  defaultActiveIndex: PropTypes.number,
+  children: PropTypes.arrayOf(PropTypes.element).isRequired,
+};
+CarouselColletion.defaultProps = {
+  defaultActiveIndex: 0,
+};
+export default function CarouselColletion({ children, defaultActiveIndex }) {
+  const [activeIndex, setActiveIndex] = useState(defaultActiveIndex);
+
+  const childrenLength = React.Children.count(children);
+  let beforeIndex = activeIndex - 1;
+  let nextIndex = activeIndex + 1;
+  if (beforeIndex < 0) {
+    beforeIndex = childrenLength - 1;
+  }
+  if (nextIndex >= childrenLength) {
+    nextIndex = 0;
+  }
+
+  return (<>
+    <div className="cc-wrapper">
+      <div className="cc-list">
+        {React.Children.map(children, (child, index) => {
+          let newClass = '';
+          if (index === activeIndex) {
+            newClass = 'ani-active';
+          } else if (index === beforeIndex) {
+            newClass = 'ani-before'
+          } else if (index === nextIndex) {
+            newClass = 'ani-next'
+          }
+          return React.cloneElement(child, { newClass });
+        })}
+      </div>
+
+      <div className="cc-control">
+      <button onClick={() => {
+          let newActiveIndex = activeIndex - 1;
+          if(newActiveIndex < 0) {
+            newActiveIndex = childrenLength - 1;
+          }
+          setActiveIndex(newActiveIndex);
+        }}>Before</button>
+        <button onClick={() => {
+          let newActiveIndex = activeIndex + 1;
+          if (newActiveIndex >= childrenLength) {
+            newActiveIndex = 0;
+          }
+          setActiveIndex(newActiveIndex);
+        }}>Next</button>
+      </div>
+    </div>
+  </>);
+}
