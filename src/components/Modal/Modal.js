6fef1f0179cdd7d0921b9162579e3c2252db2739
JianFengH <707156268@qq.com>
2022-07-26 23:42:13 -0700

    Use ReactDom.createPortal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import * as ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import './Modal.css';
+
+const MODAL_ROOT_ID = 'modal-root';
+function getModalRootElement() {
+  let root = document.getElementById(MODAL_ROOT_ID);
+  if (!root) {
+    root = document.createElement('div');
+    root.id = MODAL_ROOT_ID;
+    document.body.appendChild(root);
+  }
+  return root;
+}
 
 Modal.propTypes = {
   visible: PropTypes.bool.isRequired,
@@ -12,18 +24,23 @@ Modal.defaultProps = {
 };
 
 export default function Modal({ visible, onClose, children }) {
+  const modalRootEl = getModalRootElement();
+
   if (!visible) {
     return;
   }
-  return <div className='modal-wrapper'>
-    <div className='modal-mask' onClick={() => onClose()}></div>
-    <div className='modal-content'>
-      <div className='modal-header'>
-        <button onClick={() => onClose()}>Close</button>
-      </div>
-      <div className='modal-body'>
-        {children}
+  return ReactDOM.createPortal(
+    <div className='modal-wrapper'>
+      <div className='modal-mask' onClick={() => onClose()}></div>
+      <div className='modal-content'>
+        <div className='modal-header'>
+          <button onClick={() => onClose()}>Close</button>
+        </div>
+        <div className='modal-body'>
+          {children}
+        </div>
       </div>
     </div>
-  </div>
+    , modalRootEl);
+
 }
